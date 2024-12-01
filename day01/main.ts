a6d20bfc6a2fc742dbebfbca18f1a068d75e9c89
Carlos Eduardo Veras Neves <carlos.neves@cade.gov.br>
2024-12-01 20:39:06 -0300

    finished day01

--- day01/main.ts
+++ day01/main.ts
@@ -10,15 +10,61 @@ async function readCsvFile(filePath: string) {
     skipFirstRow: false, // Skip the header row if your CSV has headers
   });
 
-  return records;
+  const arr: Array<number[]> = [];
+  
+  records.forEach((record) => {
+    
+    let line: number[] = [];
+
+    record[0] = record[0].replace(/\s+/g, ",").split(",");
+    
+    line = record[0].map((num:string) => Number(num));
+        
+    arr.push(line);
+    
+  });
+
+  let arr_left: number[] = [];
+  for(let i = 0; i < arr.length; i++){
+    arr_left.push(arr[i][0]);
+  }
+
+  let arr_right: number[] = [];
+  for(let i = 0; i < arr.length; i++){
+    arr_right.push(arr[i][1]);
+  }
+
+  arr_left = arr_left.sort()
+  arr_right = arr_right.sort()
+    
+  return [arr_left, arr_right];
 }
 
-// Call the function with your CSV file path
-let input = readCsvFile("sample.csv");
+function calculateDistance(arr_left: number[], arr_right: number[]): Array<number> {
+  
+  let distances: Array<number> = [];
+  
+  for(let i = 0; i < arr_left.length; i++){
+    distances.push(Math.abs(arr_left[i] - arr_right[i]));
+  }
 
-input.then((data) => {
-  console.log(data);
-});
+  return distances;
+}
 
+function calculateTotalDistance(distances: Array<number>): number {
+  let totalDistance = 0;
+  
+  distances.forEach((distance) => {
+    totalDistance += distance;
+  });
+  
+  return totalDistance;
+}
 
+console.log("=== AOC 2024 - Day 01 ===");
+// read csv
+const input = await readCsvFile("data.csv");
 
+// calculate total distance
+console.log("=== Total Distance ===");
+console.log(calculateTotalDistance(calculateDistance(input[0], input[1])));
